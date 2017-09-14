d36cd779de86fc5ecd30928af97b89e438d99e87
Scott Atkins <satkins16@gmail.com>
2017-09-14 12:50:58 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,6 +5,9 @@ class Ride < ActiveRecord::Base
   def take_ride
     if self.user.tickets < self.attraction.tickets
       "Sorry. You do not have enough tickets to ride the Roller Coaster."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the Roller Coaster."
+
     end
   end
 
