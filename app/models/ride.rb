14ad56c13e920901eb2ac6cede296c85ee24130b
Scott Atkins <satkins16@gmail.com>
2017-09-15 12:53:57 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -12,6 +12,7 @@ class Ride < ActiveRecord::Base
     else
       self.user.tickets = self.user.tickets - self.attraction.tickets
       self.user.nausea = self.user.nausea + self.attraction.nausea_rating
+      self.user.happiness = self.user.happiness + self.attraction.happiness_rating
       self.user.save
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914123805) do
+ActiveRecord::Schema.define(version: 20170915125115) do
 
   create_table "attractions", force: :cascade do |t|
     t.string   "name"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20170914123805) do
     t.integer  "nausea"
     t.integer  "height"
     t.integer  "tickets"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
