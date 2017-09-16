9d453fbaf97e7ec267351cf01172580723500f01
Scott Atkins <satkins16@gmail.com>
2017-09-16 13:14:34 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,12 @@
 class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+    @user = User.find(session[:user_id])
+  end
+
 end
